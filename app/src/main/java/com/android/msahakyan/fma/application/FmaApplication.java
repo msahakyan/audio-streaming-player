4b9ff8bc099e87384a3c33d3bf3fa88f2c8409b8
msahakyan <msahakyan.work@gmail.com>
2016-11-07 00:18:55 +0100

    Removed unused code from FmaApplication class.

--- app/src/main/java/com/android/msahakyan/fma/application/FmaApplication.java
+++ app/src/main/java/com/android/msahakyan/fma/application/FmaApplication.java
@@ -50,36 +50,4 @@ public class FmaApplication extends Application {
     public static FmaApplication get(Context context) {
         return (FmaApplication) context.getApplicationContext();
     }
-
-//    public RequestQueue getRequestQueue() {
-//        if (mRequestQueue == null) {
-//            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
-//        }
-//
-//        return mRequestQueue;
-//    }
-
-//    public ImageLoader getImageLoader() {
-//        getRequestQueue();
-//        if (mImageLoader == null) {
-//            mImageLoader = new ImageLoader(this.mRequestQueue, BitmapLruCache.getInstance());
-//        }
-//        return this.mImageLoader;
-//    }
-
-//    public <T> void addToRequestQueue(Request<T> req, String tag) {
-//        // set the default tag if tag is empty
-//        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-//        getRequestQueue().add(req);
-//    }
-//
-//    public <T> void addToRequestQueue(Request<T> req) {
-//        addToRequestQueue(req, null);
-//    }
-
-//    public void cancelPendingRequests(Object tag) {
-//        if (mRequestQueue != null) {
-//            mRequestQueue.cancelAll(tag);
-//        }
-//    }
 }
